931c33b9af9375a8ad78187bae2541c41665f72d
prathibha <pratibha.chitti@gmail.com>
2021-09-21 18:38:54 +0530

    Added two files

--- test.py
+++ test.py
@@ -2,9 +2,12 @@
 
 num1 = 1.5
 num2 = 6.3
+# Add num1 and num2
+#hello python
 
 # Add two numbers
 sum = num1 + num2
 
 # Display the sum
 print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
+ 
